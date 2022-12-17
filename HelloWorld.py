79de2686a805debef4e6e4ceb569a16084342402
Suraj Jagtap <suraj552003@gmail.com>
2022-12-17 14:59:43 +0530

    till functions

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,7 @@
-# This program prints Hello, world!
-
-print('Hello, world')
-
-# Path: HelloWorld.py
-# This program prints Hello, world!
+#sum of first n natural nos. using while loop
+n = int(input());
+i = 1;
+sum1 = 0;
+while (i < n):
+    sum1 = sum1 + i
+print(sum1);
