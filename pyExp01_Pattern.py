8808d99ebf3459d2fbe37592015b18c243367b9f
Suraj Jagtap <suraj552003@gmail.com>
2023-01-27 16:42:07 +0530

    exp01

---
+++ pyExp01_Pattern.py
@@ -0,0 +1,28 @@
+# SE-3, 20, Suraj Jagtap
+# Write a program to implement the patterns given in the attachment
+
+#Patter No. 01
+n = 5
+for i in range(n): #Rows
+    for j in range(n): #Columns
+        if i == n//2 or j == n//2: 
+            print("*", end = " ")
+        else:
+            print(" ", end = " ") 
+    print()
+
+# n//2 is the middle row and column of the matrix
+
+print()
+
+#Patter No. 02
+n = 5
+for i in range(n):
+    for j in range(n):
+        if i == j or i + j == n - 1:
+            print("*", end = " ")
+        else:
+            print(" ", end = " ")
+    print()
+
+# i = j & i + j = n - 1 are the diagonal elements of the matrix
